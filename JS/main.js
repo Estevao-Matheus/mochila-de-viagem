eabb8b83d15b8f05d1726c94f1382822a7472bef
Matheus Estevão <matheus935@gmail.com>
2023-07-09 02:02:12 -0300

    adiciona itens a lista via JS

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista")
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+
+    criaElemento (e.target.elements['nome'].value, e.target.elements['quantidade'].value);
+
+})
+
+function criaElemento (nome, quantidade) {
+    console.log(nome);
+    console.log(quantidade);
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+
+}
