f4c0b27cf952e056a829b7f1d5c13f6cd5d5ff01
Matheus Estevão <matheus935@gmail.com>
2023-07-17 23:18:15 -0300

    salva itens e atualiza do local storage para html

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,47 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+
+itens.forEach( (element) => {
+   criaElemento(element);
+});
 
 form.addEventListener("submit", (e)=> {
     e.preventDefault()
+    
+    const nome = e.target.elements['nome']
+    const quantidade = e.target.elements['quantidade']
 
-    criaElemento (e.target.elements['nome'].value, e.target.elements['quantidade'].value);
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento (itemAtual);
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value="";
+    quantidade.value="";
 
 
 })
 
-function criaElemento (nome, quantidade) {
-    console.log(nome);
-    console.log(quantidade);
-
+function criaElemento (item) {
+ 
+    console.log(item.nome, item.quantidade)
+    
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
-
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual)
-
-    localStorage.setItem("ïtem", JSON.stringify(itens))
-
 }
