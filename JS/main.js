5c96896eb7cc1395af45dfa1506b8154bad72f06
Matheus Estevão <matheus935@gmail.com>
2023-07-20 04:05:11 -0300

    atualiza quantidade de itens

--- JS/main.js
+++ JS/main.js
@@ -12,15 +12,29 @@ form.addEventListener("submit", (e)=> {
     
     const nome = e.target.elements['nome']
     const quantidade = e.target.elements['quantidade']
+     
+    const existe = itens.find (e => e.nome === nome.value)
 
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento (itemAtual);
-
-    itens.push(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id;
+        console.log(existe.id);
+        atualizaElemento(itemAtual);
+        console.log(existe)
+    }else {
+        
+        itemAtual.id = itens.lenght;
+        criaElemento (itemAtual);
+        itens.push(itemAtual)
+        console.log(itens.lenght);   
+    }
+   
+      
+    
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -32,16 +46,21 @@ form.addEventListener("submit", (e)=> {
 
 function criaElemento (item) {
  
-    console.log(item.nome, item.quantidade)
-    
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
+}
+
+function atualizaElemento (item) {
+
+    console.log (item);
+    document.querySelector("[data-id='"+ item.id+"']").innerHTML = item.quantidade;
 }
