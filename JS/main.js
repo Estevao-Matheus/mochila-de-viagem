0c8de1f137107252540f1bb8dd56e75ffa8ddaf6
Matheus Estevão <matheus935@gmail.com>
2023-07-21 04:34:57 -0300

    remove itens da pagina e localstorage

--- JS/main.js
+++ JS/main.js
@@ -22,15 +22,18 @@ form.addEventListener("submit", (e)=> {
 
     if (existe) {
         itemAtual.id = existe.id;
-        console.log(existe.id);
+    
         atualizaElemento(itemAtual);
-        console.log(existe)
+    
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     }else {
         
-        itemAtual.id = itens.lenght;
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+        
         criaElemento (itemAtual);
+        
         itens.push(itemAtual)
-        console.log(itens.lenght);   
+   
     }
    
       
@@ -55,6 +58,8 @@ function criaElemento (item) {
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
+    
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem);
 }
@@ -63,4 +68,23 @@ function atualizaElemento (item) {
 
     console.log (item);
     document.querySelector("[data-id='"+ item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
